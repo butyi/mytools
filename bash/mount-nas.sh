e849e44be503584c2da6ae8772740302b5ce35e8
BENCSIK Janos <git@butyi.hu>
2014-08-24 22:27:00 +0200

    real user and pass from file

--- bash/mount-nas.sh
+++ bash/mount-nas.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Read my NAS account data (NAS_USER, NAS_PASS)
-#. ~/.my-accounts
+# Read my NAS account data
+~/.my-accounts
 
 # Mount NAS
-sudo mount -t cifs //192.168.0.134/Volume_1 ~/nas -o username=butyi,password=xxxxx
+sudo mount -t cifs //192.168.0.134/Volume_1 ~/nas -o username=$NASUSER,password=$NASPASS
 
